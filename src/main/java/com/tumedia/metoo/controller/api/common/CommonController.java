fd52820f50fba6e327221643cc07ae39c46479ed
xximon <yr1633@gmail.com>
2020-10-20 16:00:51 +0900

    공통 기능(로그인,회원가입,초기데이터 등) 컨트롤러 추가

---
+++ src/main/java/com/tumedia/metoo/controller/api/common/CommonController.java
@@ -0,0 +1,39 @@
+package com.tumedia.metoo.controller.api.common;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tumedia.metoo.common.data.Response;
+import com.tumedia.metoo.dao.member.MemberDAO;
+
+import lombok.RequiredArgsConstructor;
+
+/**
+ * 공통 기능 컨트롤러(회원가입, 로그인, 초기데이터 등)
+ */
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api")
+public class CommonController {
+	
+	@Autowired
+	private MemberDAO memberDAO;
+
+	
+	// 초기데이터
+	@PostMapping("/data")
+	public Response<Map<String, Object>> data() {
+		//TODO: 초기데이터 설계 및 생성
+		HashMap<String, Object> param = new HashMap<>();
+
+		return new Response(param);
+	}
+	// 세션 생성
+	
+	// 세션 정보 받아오
+}
