6afbad9e3cb37956a02d92bbc9817e6fee6b124d
xximon <yr1633@gmail.com>
2020-10-20 23:05:48 +0900

    signup 수정, 공통 응답 클래스 주석 수정

--- src/main/java/com/tumedia/metoo/common/data/Response.java
+++ src/main/java/com/tumedia/metoo/common/data/Response.java
@@ -2,6 +2,14 @@ package com.tumedia.metoo.common.data;
 
 import lombok.Data;
 
+
+/**
+ * [공통] 응답 클래스
+ * 
+ * @author ximon
+ * 
+ * @param <T>
+ */
 @Data
 public class Response<T> {
     private int statusCode;
--- src/main/java/com/tumedia/metoo/controller/api/member/MemberController.java
+++ src/main/java/com/tumedia/metoo/controller/api/member/MemberController.java
@@ -9,6 +9,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,8 +31,10 @@ public class MemberController {
 	private MemberDAO memberDAO;
 
 	// 회원가입
-	@GetMapping("/signup")
-	public Response<Member> signUp(@Valid @ModelAttribute() MemberDTO memberDTO, 
+	@PostMapping("/signup")
+	public Response<Member> signUp(
+			@Valid @ModelAttribute MemberDTO memberDTO,
+//			@RequestBody MemberDTO memberDTO,
 								   BindingResult result,
 								   HttpServletRequest request, 
 								   HttpServletResponse response) {
@@ -44,44 +47,44 @@ public class MemberController {
 //		Member record = memberDAO.selectOneMember(member); // 회원 단건 조회
 		Member record = null;
 
-//		if (record == null) {
-//			// 신규 회원가입
-//			member = new Member();
-//			member.setMemberId(memberDTO.getMemberId());
-//			member.setPassword(memberDTO.getPassword());
-//			member.setName(memberDTO.getName());
-//			member.setGrade(memberDTO.getGrade());
-//			member.setGender(memberDTO.getGender());
-//			member.setEmail(memberDTO.getEmail());
-//			member.setComName(memberDTO.getComName());
-//			member.setDeptNo(memberDTO.getDeptNo());
-//			member.setLecNo(memberDTO.getLecNo());
-//			member.setLecNo1(memberDTO.getLecNo1());
-//			member.setLecNo2(memberDTO.getLecNo2());
-//			member.setLecNo3(memberDTO.getLecNo3());
-//			member.setLecNo4(memberDTO.getLecNo4());
-//			member.setLecNo5(memberDTO.getLecNo5());
-//			memberDAO.signUp(member);
-//			res.setData(member);
-//		} else {
-//			// 탈퇴 회원 재가입 (회원탈퇴 기능 현재 계획에 없음)
-//		}
-		member.setMemberId("1111");
-		member.setPassword("2222");
-		member.setName("테스트계정");
-		member.setGrade("1");
-		member.setGender("1");
-		member.setEmail("test@test.test");
-		member.setComName("공과대학");
-		member.setDeptNo("11");
-		member.setLecNo("999");
-		member.setLecNo1("101");
-		member.setLecNo2("202");
-		member.setLecNo3("303");
-		member.setLecNo4("404");
-		member.setLecNo5("505");
-		memberDAO.signUp(member);
-		res.setData(member);
+		if (record == null) {
+			// 신규 회원가입
+			member = new Member();
+			member.setMemberId(memberDTO.getMemberId());
+			member.setPassword(memberDTO.getPassword());
+			member.setName(memberDTO.getName());
+			member.setGrade(memberDTO.getGrade());
+			member.setGender(memberDTO.getGender());
+			member.setEmail(memberDTO.getEmail());
+			member.setComName(memberDTO.getComName());
+			member.setDeptNo(memberDTO.getDeptNo());
+			member.setLecNo(memberDTO.getLecNo());
+			member.setLecNo1(memberDTO.getLecNo1());
+			member.setLecNo2(memberDTO.getLecNo2());
+			member.setLecNo3(memberDTO.getLecNo3());
+			member.setLecNo4(memberDTO.getLecNo4());
+			member.setLecNo5(memberDTO.getLecNo5());
+			memberDAO.signUp(member);
+			res.setData(member);
+		} else {
+			// 탈퇴 회원 재가입 (회원탈퇴 기능 현재 계획에 없음)
+		}
+//		member.setMemberId("1111");
+//		member.setPassword("2222");
+//		member.setName("테스트계정");
+//		member.setGrade("1");
+//		member.setGender("1");
+//		member.setEmail("test@test.test");
+//		member.setComName("공과대학");
+//		member.setDeptNo("11");
+//		member.setLecNo("999");
+//		member.setLecNo1("101");
+//		member.setLecNo2("202");
+//		member.setLecNo3("303");
+//		member.setLecNo4("404");
+//		member.setLecNo5("505");
+//		memberDAO.signUp(member);
+//		res.setData(member);
 		return res;
 	}
 }
