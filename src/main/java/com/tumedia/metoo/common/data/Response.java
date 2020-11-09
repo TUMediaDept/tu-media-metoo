da17b2add1df9e4f62de3a51a02187b5b288b5f1
김창환 <63185486+rlackdghks@users.noreply.github.com>
2020-11-09 15:38:37 +0900

    20.11.08 15:38 강의 평가 commit

--- src/main/java/com/tumedia/metoo/common/data/Response.java
+++ src/main/java/com/tumedia/metoo/common/data/Response.java
@@ -22,29 +22,5 @@ public class Response<T> {
         this.data = data;
     }
 
-	public int getStatusCode() {
-		return statusCode;
-	}
-
-	public void setStatusCode(int statusCode) {
-		this.statusCode = statusCode;
-	}
-
-	public String getMessage() {
-		return message;
-	}
-
-	public void setMessage(String message) {
-		this.message = message;
-	}
-
-	public T getData() {
-		return data;
-	}
-
-	public void setData(T data) {
-		this.data = data;
-	}
-    
-    
+	
 }
--- src/main/java/com/tumedia/metoo/model/notice/Notice.java
+++ src/main/java/com/tumedia/metoo/model/notice/Notice.java
@@ -8,23 +8,32 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 
-@Data
+
 @NoArgsConstructor
 @AllArgsConstructor
+@Data
 public class Notice implements Serializable {
 	
 private static final long serialVersionUID = -7346858923123383239L;
 	
 	public static final String ANDORID = "1";
+	
 	public static final String IOS = "2";
 	
 	private int id;
+	
 	private String title;
+	
 	private String content;
+	
 	private String name;
+	
 	private String noticeDvcd;
+	
 	private String fixYn;
+	
 	private int viewCnt;
+	
 	private LocalDate regDate;
 	
 	
